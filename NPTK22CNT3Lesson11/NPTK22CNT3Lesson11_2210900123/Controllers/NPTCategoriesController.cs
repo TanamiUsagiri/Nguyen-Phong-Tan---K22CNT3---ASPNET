9a4651334fa9d3ff5c54eccffd26cda891d4bb6e
TanamiUsagiri <tanamiusagiri@gmail.com>
2024-07-04 11:33:15 +0700

    Lesson11

---
+++ NPTK22CNT3Lesson11/NPTK22CNT3Lesson11_2210900123/Controllers/NPTCategoriesController.cs
@@ -0,0 +1,127 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NPTK22CNT3Lesson11_2210900037.Models;
+
+namespace NPTK22CNT3Lesson11_2210900037.Controllers
+{
+    public class NPTCategoriesController : Controller
+    {
+        private NptK22CNT3_Lesson11Entities db = new NptK22CNT3_Lesson11Entities();
+
+        // GET: NPTCategories
+        public ActionResult NPTIndex()
+        {
+            return View(db.NPTCategories.ToList());
+        }
+
+        // GET: NPTCategories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NPTCategory lHLCategory = db.NPTCategories.Find(id);
+            if (lHLCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lHLCategory);
+        }
+
+        // GET: NPTCategories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NPTCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NptId,NptCateName,NptStatus")] NPTCategory lHLCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NPTCategories.Add(lHLCategory);
+                db.SaveChanges();
+                return RedirectToAction("NPTIndex");
+            }
+
+            return View(lHLCategory);
+        }
+
+        // GET: NPTCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NPTCategory lHLCategory = db.NPTCategories.Find(id);
+            if (lHLCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lHLCategory);
+        }
+
+        // POST: NPTCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NptId,NptCateName,NptStatus")] NPTCategory lHLCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lHLCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("NPTIndex");
+            }
+            return View(lHLCategory);
+        }
+
+        // GET: NPTCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NPTCategory lHLCategory = db.NPTCategories.Find(id);
+            if (lHLCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lHLCategory);
+        }
+
+        // POST: NPTCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            NPTCategory lHLCategory = db.NPTCategories.Find(id);
+            db.NPTCategories.Remove(lHLCategory);
+            db.SaveChanges();
+            return RedirectToAction("NPTIndex");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NPTK22CNT3Lesson11/NPTK22CNT3Lesson11_2210900123/Controllers/NPTProductsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NPTK22CNT3Lesson11_2210900037.Models;
+
+namespace NPTK22CNT3Lesson11_2210900037.Controllers
+{
+    public class NPTProductsController : Controller
+    {
+        private NptK22CNT3_Lesson11Entities db = new NptK22CNT3_Lesson11Entities();
+
+        // GET: NPTProducts
+        public ActionResult NptIndex()
+        {
+            var lHLProducts = db.NPTProducts.Include(l => l.NPTCategory);
+            return View(lHLProducts.ToList());
+        }
+
+        // GET: NPTProducts/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NPTProduct lHLProduct = db.NPTProducts.Find(id);
+            if (lHLProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lHLProduct);
+        }
+
+        // GET: NPTProducts/Create
+        public ActionResult Create()
+        {
+            ViewBag.NptCateId = new SelectList(db.NPTCategories, "NptId", "NptCateName");
+            return View();
+        }
+
+        // POST: NPTProducts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NptId2210900037,NptProName,NptQty,NptPrice,NptCateId,NptActive")] NPTProduct lHLProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NPTProducts.Add(lHLProduct);
+                db.SaveChanges();
+                return RedirectToAction("NptIndex");
+            }
+
+            ViewBag.NptCateId = new SelectList(db.NPTCategories, "NptId", "NptCateName", lHLProduct.NptCateId);
+            return View(lHLProduct);
+        }
+
+        // GET: NPTProducts/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NPTProduct lHLProduct = db.NPTProducts.Find(id);
+            if (lHLProduct == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.NptCateId = new SelectList(db.NPTCategories, "NptId", "NptCateName", lHLProduct.NptCateId);
+            return View(lHLProduct);
+        }
+
+        // POST: NPTProducts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NptId2210900037,NptProName,NptQty,NptPrice,NptCateId,NptActive")] NPTProduct lHLProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lHLProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("NptIndex");
+            }
+            ViewBag.NptCateId = new SelectList(db.NPTCategories, "NptId", "NptCateName", lHLProduct.NptCateId);
+            return View(lHLProduct);
+        }
+
+        // GET: NPTProducts/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NPTProduct lHLProduct = db.NPTProducts.Find(id);
+            if (lHLProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lHLProduct);
+        }
+
+        // POST: NPTProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            NPTProduct lHLProduct = db.NPTProducts.Find(id);
+            db.NPTProducts.Remove(lHLProduct);
+            db.SaveChanges();
+            return RedirectToAction("NptIndex");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
